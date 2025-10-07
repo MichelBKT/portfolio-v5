246252884153063608654d88cf7d1a8dd620c0c8
Michel Banckaert <michel.banckaert@epitech.eu>
2025-10-07 04:36:28 +0200

    add speedinsights

--- app/app.vue
+++ app/app.vue
@@ -3,11 +3,13 @@ import Navbar from "~/components/Navbar.vue"
 import Footer from "~/components/Footer.vue"
 import StarfieldBackground from "~/components/StarfieldBackground.vue"
 import { Analytics } from '@vercel/analytics/nuxt';
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 
 </script>
 
 <template>
   <Analytics />
+  <SpeedInsights />
   <div class="min-h-screen relative p-4 md:p-8">
     <NuxtRouteAnnouncer />
 
